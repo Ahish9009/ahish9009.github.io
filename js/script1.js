28b6d689886d561e396a45be680b93238b1d5f85
Ahish9009 <ahishjayant9009@gmail.com>
2020-06-04 23:42:29 +0400

    fixed nav-title glitch on small screens

--- js/script1.js
+++ js/script1.js
@@ -1,12 +1,12 @@
 console.log(window.scrollY);
 
 var h = $('.container').height();
-console.log(h);
+//console.log(h);
 
 $('.hobby-sec-title-container').css({top: h-1});
 
 var h2 = $('.hobby-sec-title-container').height();
 var h3 = h + h2;
-console.log('-'+h3+h2/2);
-$('.hobby').css("padding-top", h3+30);
-$('.hobby').css("margin-top",'-'+h3);
+//console.log('-'+h3+h2/2);
+$('.hobby').css("padding-top", h3+15);
+$('.adj-margin').css("margin-top",'-'+h3);
