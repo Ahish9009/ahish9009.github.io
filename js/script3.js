9e5f1e424713b9e16ac2fd853ce789c8b5964be3
Ahish9009 <ahishjayant9009@gmail.com>
2020-06-13 14:42:17 +0400

    [projects] scrolls to top on tab switch

--- js/script3.js
+++ js/script3.js
@@ -6,15 +6,19 @@ jQuery('button').click( function(e) {
 
 $('#experience').on('shown.bs.collapse', function () {
 	$('.exp-heading').css('background-color', 'rgba(49,150,246,0.8)');
+	$('.content-wrap').scrollTop(0);
 });
 $('#experience').on('hidden.bs.collapse', function () {
 	$('.exp-heading').css('background-color', 'rgba(0,0,0,1)');
 });
 $('#projects').on('shown.bs.collapse', function () {
 	$('.proj-heading').css('background-color', 'rgba(49,150,246,0.8)');
+	$('.content-wrap').scrollTop(0);
 });
 $('#projects').on('hidden.bs.collapse', function () {
 	$('.proj-heading').css('background-color', 'rgba(0,0,0,1)');
+	$('html, body').delay('200').animate({
+    }, 200);
 });
 
 $(window).resize(function() {setTilesPadding();})
